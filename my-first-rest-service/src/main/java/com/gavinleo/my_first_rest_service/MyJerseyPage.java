bbf18673dd83eae9b780dce7fa5b9003d9d5b5a9
leorhynieg <leorhynieg@172.27.6.36>
2018-08-23 16:46:34 -0400

    Add second GET method
    Change path in web.xml

--- my-first-rest-service/src/main/java/com/gavinleo/my_first_rest_service/MyJerseyPage.java
+++ my-first-rest-service/src/main/java/com/gavinleo/my_first_rest_service/MyJerseyPage.java
@@ -3,15 +3,17 @@ package com.gavinleo.my_first_rest_service;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class MyJerseyPage {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
-		return "Wha a gwaan?";
+	public String sayHtmlHello( @QueryParam("yourName") String yourName, @QueryParam("yourLocation") String yourLocation ) {
+		return "Wha a gwaan, " + yourName + " in " + yourLocation + "?";
 		
-		// http://localhost:8080/my-first-rest-service/thisthat/hello
+		//http://localhost:8080/my-first-rest-service/thisthat/hello?yourName=Gavin&yourLocation=NJ
 	}
+	
 }
